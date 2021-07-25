ffa046135bb6652cfb7f02cd25f2941fe97938ae
thinhvqnguyen <thinh.nvq.js220597@gmail.com>
2021-07-25 16:22:14 +0700

    edit unit test to force it fail on CI/CD

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 describe('Array', () => {
   describe('#indexOf()', () => {
     it('should return -1 when the value is not present', () => {
-      assert.strictEqual([1,2,3].indexOf(4), -1);
+      assert.strictEqual([1,2,3].indexOf(3), -1);
     })
   })
 })
